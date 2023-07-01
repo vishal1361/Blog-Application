26fac3a5a8a6af536a802d8de34030e9b47acd5c
admin <admin@admins-MacBook-Pro.local>
2023-07-01 19:23:07 +0530

    adding configuration file

---
+++ src/main/java/com/BlogApi/Blog/Configuration/SQLConfiguration.java
@@ -0,0 +1,19 @@
+package com.BlogApi.Blog.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+@Configuration
+public class SQLConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder
+                .create()
+                .url("jdbc:postgresql://ydpg-cig1lat9aq012et1g3ug-a:5432/blogdb_lqtr")
+                .username("blogdb_lqtr_user")
+                .password("MoyWABvESn1ZFGJmITkNRaQh4TUgKSkv")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
