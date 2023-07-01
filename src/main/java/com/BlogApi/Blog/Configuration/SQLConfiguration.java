74b22ea652f9fcb8ef51474363d45aa52a2c4146
admin <admin@admins-MacBook-Pro.local>
2023-07-01 23:23:11 +0530

    Creating tables

--- src/main/java/com/BlogApi/Blog/Configuration/SQLConfiguration.java
+++ src/main/java/com/BlogApi/Blog/Configuration/SQLConfiguration.java
@@ -10,9 +10,9 @@ public class SQLConfiguration {
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
-                .url("jdbc:postgresql://dpg-cig1lat9aq012et1g3ug-a:5432/blogdb_lqtr")
-                .username("blogdb_lqtr_user")
-                .password("MoyWABvESn1ZFGJmITkNRaQh4TUgKSkv")
+                .url("jdbc:postgresql://localhost:5432/blogDB")
+                .username("postgres")
+                .password("apple")
                 .driverClassName("org.postgresql.Driver")
                 .build();
     }
