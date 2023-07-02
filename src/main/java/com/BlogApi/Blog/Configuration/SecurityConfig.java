dacaa78939ffb06ee9a01f7e25d5c0c7db929c40
admin <admin@admins-MacBook-Pro.local>
2023-07-02 13:15:26 +0530

    fixed cors error

---
+++ src/main/java/com/BlogApi/Blog/Configuration/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.BlogApi.Blog.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import java.util.List;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
+        return http
+                .cors(Customizer.withDefaults())
+                .authorizeRequests(authorizeRequests -> authorizeRequests.antMatchers("/**").permitAll().anyRequest().authenticated())
+                .csrf().disable()
+                .build();
+    }
+}
